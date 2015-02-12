6ffd18d3f2de3db875f87b6bdf1b4d3fb4479e3c
Richard Schneider <richard@schneiderbox.net>
2015-02-11 18:29:18 -0600

    begin adding resources

--- js/resources.js
+++ js/resources.js
@@ -1,28 +1,29 @@
 game.resources = [
+	/* Graphics.
+	 * @example
+	 * {name: "example", type: "image", src: "data/img/example.png"},
+	 */
+	{type: 'image', name: 'tiles', src: 'data/img/map/tiles.png'},
 
-    /* Graphics.
-     * @example
-     * {name: "example", type:"image", src: "data/img/example.png"},
-     */
+	/* Texture Atlases
+	 * @example
+	 * {name: "texture", type: "json", src: "data/img/example_tps.json"},
+	 */
 
-    /* Texture Atlases
-     * @example
-     * {name: "texture", type: "json", src: "data/img/example_tps.json"},
-     */
+	/* Maps.
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+	  */
+	{type: 'tmx', name: 'level', src: 'data/map/level.tmx'}
 
-    /* Maps.
-     * @example
-     * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
-     * {name: "example01", type: "tmx", src: "data/map/example01.json"},
-      */
+	/* Background music.
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+	 */
 
-    /* Background music.
-     * @example
-     * {name: "example_bgm", type: "audio", src: "data/bgm/"},
-     */
-
-    /* Sound effects.
-     * @example
-     * {name: "example_sfx", type: "audio", src: "data/sfx/"}
-     */
+	/* Sound effects.
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+	 */
 ];
