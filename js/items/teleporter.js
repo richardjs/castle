f358e588c5c632dbc23d61b2aa47d3adcc423158
Richard Schneider <richard@schneiderbox.net>
2015-02-28 11:57:21 -0600

    prevent teleporting over walls

--- js/items/teleporter.js
+++ js/items/teleporter.js
@@ -1,6 +1,45 @@
 var TELEPORT_COOLDOWN = 3000;
 var TELEPORT_WARMUP = 1200;
 
+var TeleportTester = me.Entity.extend({
+	init: function(x1, y1, x2, y2){
+		var settings = {};
+		settings.width = 5;
+		settings.height = 5;
+		this._super(me.Entity, 'init', [x1, y1, settings]);
+
+		this.body.setCollisionMask(me.collision.types.WORLD_SHAPE);
+
+		this.body.addShape(new me.Rect(0, 0, settings.width, settings.height));
+
+		var distx = x2 - x1;
+		var disty = y2 - y1;
+		var angle = Math.atan2(disty, distx);
+		var distance = Math.sqrt(
+			Math.pow(distx, 2) + Math.pow(disty, 2)
+		);
+		var dx = Math.cos(angle) * 1;
+		var dy = Math.sin(angle) * 1;
+		
+		this.body.vel.x = dx;
+		this.body.vel.y = dy;
+
+		for(var i = 0; i < distance; i++){
+			this.body.update(1);
+			me.collision.check(this);
+			if(this.failed){
+				break;
+			}
+		}
+	},
+
+	onCollision: function(response, other){
+		if(other.body.collisionType === me.collision.types.WORLD_SHAPE){
+			this.failed = true;
+		}
+	}
+});
+
 var Teleporter = Item.extend({
 	'init': function(){
 		this.name = 'Mirror of Escape';
@@ -36,10 +75,22 @@ var Teleporter = Item.extend({
 		if(this.cooldown > 0 || this.teleporting){
 			return;
 		}
-		this.teleporting = true;
-		this.warmup = TELEPORT_WARMUP;
+
 		this.targetX = me.game.player.pointerPos.x;
 		this.targetY = me.game.player.pointerPos.y;
+
+		var tester = new TeleportTester(
+			me.game.player.pos.x,
+			me.game.player.pos.y,
+			this.targetX,
+			this.targetY
+		);
+		if(tester.failed){
+			return;
+		}
+
+		this.teleporting = true;
+		this.warmup = TELEPORT_WARMUP;
 		me.game.player.renderable.flicker(TELEPORT_WARMUP);
 
 		me.game.player.itemAnimation = true;
