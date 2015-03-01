af6369453ed9973a026dc92e4b713f24e34283d2
Richard Schneider <richard@schneiderbox.net>
2015-02-28 18:27:56 -0600

    adjust flame hitbox

--- js/entities/flame.js
+++ js/entities/flame.js
@@ -6,13 +6,15 @@ game.FlameEntity = me.Entity.extend({
 		settings.image = 'sprites';
 		settings.spritewidth = 32;
 		settings.spriteheight = 32;
-		settings.height = 5;
-		settings.width = 5;
+		settings.height = 10;
+		settings.width = 15;
 		settings.collisionType = 'PROJECTILE_OBJECT';
 
 		this._super(me.Entity, 'init', [x - settings.width/2, y - settings.height/2, settings]);
 		this.body.addShape(new me.Rect(0, 0, settings.width, settings.height));
 		this.body.setCollisionMask(me.collision.types.WORLD_SHAPE | me.collision.types.ENEMY_OBJECT);
+
+		this.anchorPoint.set(.5, .75);
 
 		var angle = Math.atan2(targetY - y, targetX - x);
 		if(anglemod){
@@ -27,7 +29,7 @@ game.FlameEntity = me.Entity.extend({
 		this.damage = 3;
 		this.magic = true;
 
-		this.renderable.addAnimation('flame', [147, 148, 149], 10);
+		this.renderable.addAnimation('flame', [147, 148, 149, 148], 25);
 		this.renderable.setCurrentAnimation('flame');
 		this.renderable.setAnimationFrame(Math.floor(Math.random() * 3));
 	},
